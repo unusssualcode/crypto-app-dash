12b08a52d202684fd1be86af1c6babe3073383de
Denis <122015463+iziKomSD@users.noreply.github.com>
2024-10-22 18:52:00 +0300

    Started Content Section

--- src/components/layout/AppContent.jsx
+++ src/components/layout/AppContent.jsx
@@ -1,8 +1,9 @@
-import { Layout } from "antd";
+import { Layout, Typography } from "antd";
+import { useCrypto } from "../../context/crypto-context.jsx";
 
 const contentStyle = {
   textAlign: "center",
-  minHeight: "calc(100vh - 60px",
+  minHeight: "calc(100vh - 60px)",
   lineHeight: "120px",
   color: "#fff",
   backgroundColor: "#001529",
@@ -10,5 +11,24 @@ const contentStyle = {
 };
 
 export default function AppContent() {
-  return <Layout.Content style={contentStyle}>Content</Layout.Content>;
+  const { assets, crypto } = useCrypto();
+
+  const cryptoPriceMap = crypto.reduce((acc, c) => {
+    acc[c.id] = c.price;
+    return acc;
+  }, {});
+  return (
+    <Layout.Content style={contentStyle}>
+      <Typography.Title level={2} style={{ textAlign: "left", color: "white" }}>
+        Portfolio:{" "}
+        {assets
+          .map((asset) => {
+            return asset.amount * cryptoPriceMap[asset.id];
+          })
+          .reduce((acc, v) => (acc += v), 0)
+          .toFixed(2)}
+        $
+      </Typography.Title>
+    </Layout.Content>
+  );
 }
--- src/components/layout/AppHeader.jsx
+++ src/components/layout/AppHeader.jsx
@@ -19,7 +19,7 @@ const headerStyle = {
 export default function AppHeader() {
   const [select, setSelect] = useState(false);
   const [modal, setModal] = useState(false);
-  const [drawer, setDrawer] = useState(true);
+  const [drawer, setDrawer] = useState(false);
   const [coin, setCoin] = useState(null);
   const { crypto } = useCrypto();
 
